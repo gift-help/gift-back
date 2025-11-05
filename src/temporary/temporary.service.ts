97f28656d5ae0ba721625270b7bb899c17cc3347
flexiG17 <mister23krut@gmail.com>
2025-11-05 15:14:39 +0500

    Тестирование деплоя изменений на сервер

--- src/temporary/temporary.service.ts
+++ src/temporary/temporary.service.ts
@@ -3,14 +3,16 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TemporaryService {
   async generateGifts() {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         resolve([
           { id: 1, name: 'Плюшевый мишка', price: 1229.99 },
           { id: 2, name: 'Набор лего', price: 5449.99 },
           { id: 3, name: 'Машинки', price: 539.99 },
+          { id: 4, name: 'Машинки v2', price: 1539.99 },
+          { id: 5, name: 'Lada Kalina 2008', price: 165000 },
         ]);
-      }, 5000);
+      }, 3000);
     });
   }
 }
