624858f0c3ecf885bc5aa5adb2e20db93278f91f
flexiG17 <mister23krut@gmail.com>
2025-11-08 19:38:41 +0500

    Удалены неиспользуемые запросы, рефакторинг

--- src/temporary/temporary.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TemporaryService } from './temporary.service';
-import { TemporaryController } from './temporary.controller';
-
-@Module({
-  controllers: [TemporaryController],
-  providers: [TemporaryService],
-})
-export class TemporaryModule {}
